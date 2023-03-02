517dae0ce3105e3f8df664d52c4861d52eb0ac99
Ket Wong <78025989+ket-gresearch@users.noreply.github.com>
2023-03-02 22:11:28 +0000

    Update win-config.ps1

--- win-config.ps1
+++ win-config.ps1
@@ -4,6 +4,9 @@ Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 # Enable Containers feature
 Enable-WindowsOptionalFeature -Online -FeatureName containers -All
 
+# Restart the system to apply changes
+Restart-Computer -Force
+
 # Download the Docker installer
 Invoke-WebRequest "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe" -OutFile "DockerInstaller.exe"
 
@@ -12,3 +15,27 @@ Start-Process -FilePath ".\DockerInstaller.exe" -ArgumentList "install --quiet" 
 
 # Verify that Docker is installed
 docker version
+
+# Restart the system to apply changes
+Restart-Computer -Force
+
+# Install the Hyper-V role
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+
+# Enable Containers feature
+Enable-WindowsOptionalFeature -Online -FeatureName containers -All
+
+# Restart the system to apply changes
+Restart-Computer -Force
+
+# Download the Docker installer
+Invoke-WebRequest "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe" -OutFile "DockerInstaller.exe"
+
+# Install Docker
+Start-Process -FilePath ".\DockerInstaller.exe" -ArgumentList "install --quiet" -Wait
+
+# Verify that Docker is installed
+docker version
+
+# Restart the system to apply changes
+Restart-Computer -Force
